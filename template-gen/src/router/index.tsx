0bb504fc4b045fab1efd013cc48ad391a0f8ff18
longpham <hoanglongfpthn@gmail.com>
2026-05-06 17:26:01 +0700

    fix: entity route page

--- template-gen/src/router/index.tsx
+++ template-gen/src/router/index.tsx
@@ -1,24 +1,25 @@
-﻿import { createBrowserRouter } from "react-router-dom";
+﻿import {createBrowserRouter} from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
 import Login from "../page/auth/Login";
 import ItemPage from "@/page/item/Index";
+import {EntityPage} from "@/page/entity";
 
 export const router = createBrowserRouter([
     {
         path: "/login",
-        element: <Login />,
+        element: <Login/>,
     },
     {
         // Tất cả routes cần auth nằm trong đây
-        element: <ProtectedRoute />,
+        element: <ProtectedRoute/>,
         children: [
-            { path: "/",        element: <div>Dashboard</div> },
-            { path: "/item",    element: <ItemPage></ItemPage> },
-            { path: "/dungeon", element: <div>Dungeon</div> },
-            { path: "/entity",  element: <div>Entity</div> },
-            { path: "/loottable", element: <div>Loot Table</div> },
-            { path: "/room",    element: <div>Room</div> },
-            { path: "/spawner", element: <div>Spawner</div> },
+            {path: "/", element: <div>Dashboard</div>},
+            {path: "/item", element: <ItemPage></ItemPage>},
+            {path: "/dungeon", element: <div>Dungeon</div>},
+            {path: "/entity", element: <EntityPage></EntityPage>},
+            {path: "/loottable", element: <div>Loot Table</div>},
+            {path: "/room", element: <div>Room</div>},
+            {path: "/spawner", element: <div>Spawner</div>},
         ],
     },
 ]);
